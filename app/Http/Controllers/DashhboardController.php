41d058ad19fec52f7087fa789a5c0587129db6f1
yomangit <106853745+yomangit@users.noreply.github.com>
2025-05-22 12:06:46 +0800

    qww

--- app/Http/Controllers/DashhboardController.php
+++ app/Http/Controllers/DashhboardController.php
@@ -4,12 +4,11 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\View\View;
+
 class DashhboardController extends Controller
 {
     public function index()
     {
-        return view('dashboard.index', [
-          
-        ])->extends('base.index', ['header' => 'Dashboard', 'title' => 'Dashboard'])->section('content');
+        return view('dashboard.index', [])->extends('base.index', ['header' => 'Dashboard', 'title' => 'Dashboard'])->section('content');
     }
 }
